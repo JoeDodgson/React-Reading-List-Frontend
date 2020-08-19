074e2ebb27bb27f1c4590d8ee3b276c86af88234
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-19 10:33:00 +0100

    Updated searchBar component to use material UI button

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
 
 class SearchBar extends Component {
     // Setting the component's initial state
@@ -32,26 +33,28 @@ class SearchBar extends Component {
     render() {
       // Notice how each input has a `value`, `name`, and `onChange` prop
         return (
-                <div className="row searchbar-container">
-                    <div className="col-10" style={{margin: "0 auto"}}>
-                        <form className="form">
-                            <div className="row">
-                                <div className="col-1"></div>
-                                <input
-                                className="col-7"
-                                value={this.state.search}
-                                name="search"
-                                onChange={this.handleInputChange}
-                                type="text"
-                                placeholder="Search for a book"
-                                />
-                                <div className="col-1"></div>
-                                <button id="search-button" className="col-2" onClick={this.handleFormSubmit}>Submit</button>
-                                <div className="col-1"></div>
-                            </div>
-                        </form>
-                    </div>
+            <div className="row searchbar-container">
+                <div className="col-10" style={{margin: "0 auto"}}>
+                    <form className="form">
+                        <div className="row">
+                            <div className="col-1"></div>
+                            <input
+                            className="col-7"
+                            value={this.state.search}
+                            name="search"
+                            onChange={this.handleInputChange}
+                            type="text"
+                            placeholder="Search for a book"
+                            />
+                            <div className="col-1"></div>
+                            <Button id="search-button" className="col-2" onClick={this.handleFormSubmit} variant="contained" color="primary">
+                                Submit
+                            </Button>
+                            <div className="col-1"></div>
+                        </div>
+                    </form>
                 </div>
+            </div>
         );
     }
 }
