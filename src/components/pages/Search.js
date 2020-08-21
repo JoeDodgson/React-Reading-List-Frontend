939c792f2bac5d2ec217f9e550d76cff977d7ffa
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-21 14:55:25 +0100

    Search.js page - Passed search and update state functions into props
    -Imported in googleBooksAPI.js
    -Defined functions for handle change event and handle submit form event (using Google Books API call method)
    -Passed functions and search results into child components

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavBar } from '../NavBar'
 import { Header } from '../Header'
 import SearchBar from '../SearchBar'
 import { BooksContainer } from '../BooksContainer'
+import API from '../../utils/googleBooksAPI'
 
-export const Search = () => {
-  return (
-    <>
-        <NavBar active="search"/>
-        <Header title="Results"/>
-        <SearchBar />
-        <BooksContainer heading="Search Results"/>
-    </>
-  );
+class Search extends Component {
+  state = {
+    search: "",
+    results: [],
+    error: ""
+  };
+
+  // When the component mounts, get a list of all available base breeds and update this.state.breeds
+  handleInputChange = event => {
+    this.setState({ search: event.target.value });
+  };
+  
+  handleFormSubmit = async event => {
+    event.preventDefault();
+    try {
+      const results = await API.getBookBySearch(this.state.search);
+      this.setState({ ...this.state, results });
+      console.log(results);
+    }
+    catch (err) {
+      console.log(`ERROR: Search.js - handleFormSubmit() - ${err}`);
+    }
+  };
+
+  render() {
+    return (
+      <>
+          <NavBar active="search"/>
+          <Header title="Results"/>
+          <SearchBar             
+            handleInputChange={this.handleInputChange}
+            handleFormSubmit={this.handleFormSubmit}
+          />
+          <BooksContainer 
+            heading="Search Results"
+            search={this.state.search}
+            books={this.state.results}
+          />
+      </>
+    );
+  }
 }
 
 export default Search;
