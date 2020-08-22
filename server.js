9550751cb0def2ec7e7a5d1587179fb0be0f2801
Joe Dodgson <j.dodgson@live.co.uk>
2020-08-22 19:19:55 +0100

    Set up route API handling in server

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// Add API routes and view
+app.use(routes);
+
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
